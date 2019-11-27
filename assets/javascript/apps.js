1e7b2f04d6556535aa5de7a266dc272028a34eec
Michael Gelber <mpgelber7495@gmail.com>
2019-11-27 10:11:40 -0500

    Half functional band member page

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,3 +1,12 @@
 $(".member-headshot").click(function(event) {
-  console.log(event.target);
+  var name = event.target.id;
+  $("member-headshot").css("opacity", "0.1");
+  $(`#${name}`).css("opacity", "0.5");
+  var memberObject = members.filter(member => member["name"] === name)[0];
+  var memberElement = $("<div>");
+  var memberPic = `<img src = ${memberObject["actionShotURL"]} class = "w-50">`;
+  memberElement.append(memberPic);
+  var memberName = `<h3 class = "member-name-active"> ${memberObject["name"]}</h3>`;
+  memberElement.append(memberName);
+  $(".selected-character-content-holder").html(memberElement);
 });
