492c5ce8e966caa40eeadfbf547f65be2ed4d9c3
Michael Gelber <mpgelber7495@gmail.com>
2019-11-27 10:40:27 -0500

    Almost completed functionality of the band members page

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,12 +1,16 @@
 $(".member-headshot").click(function(event) {
   var name = event.target.id;
-  $("member-headshot").css("opacity", "0.1");
-  $(`#${name}`).css("opacity", "0.5");
+  $(".member-headshot").css("opacity", ".1");
+  $(`#${name}`).css("opacity", "0.8");
+  $(".member-headshot").addClass("inactive-member");
   var memberObject = members.filter(member => member["name"] === name)[0];
-  var memberElement = $("<div>");
-  var memberPic = `<img src = ${memberObject["actionShotURL"]} class = "w-50">`;
-  memberElement.append(memberPic);
-  var memberName = `<h3 class = "member-name-active"> ${memberObject["name"]}</h3>`;
+  var memberElement = $(".selected-character-content-holder");
+  memberElement.html("");
+  var memberName = `<div class = "row"> <div class="col-12 d-flex justify-content-center"><h3 class = "member-name-active"> ${memberObject["name"]}</h3> </div></div>`;
   memberElement.append(memberName);
-  $(".selected-character-content-holder").html(memberElement);
+
+  var memberInstruments = `<div class = "row"> <div class="col-12 d-flex justify-content-center"><h3 class = "member-instrument-active"> Instruments: ${memberObject["instrument"]}</h3> </div></div>`;
+  memberElement.append(memberInstruments);
+  var memberPic = `<div class = "row"> <div class="col-12 d-flex justify-content-center"> <img src = ${memberObject["actionShotURL"]} class = "w-50"> </div> </div>`;
+  memberElement.append(memberPic);
 });
