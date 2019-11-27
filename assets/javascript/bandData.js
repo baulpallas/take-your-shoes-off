86b384b33b0c5888ae3c6c5718e46d00d7b7f47f
Michael Gelber <mpgelber7495@gmail.com>
2019-11-27 09:39:49 -0500

    Working on band page

---
+++ assets/javascript/bandData.js
@@ -0,0 +1,9 @@
+var members = [
+  {
+    name: "Rafi",
+    instrument: "Guitar and Vocals",
+    actionShotURL: "url",
+    bio:
+      "Rafi is a true musical prodigy. He's been playing every instrument since the day he was born. Rafi's passions are rooted in the funk but he gets groovy to all forms of music."
+  }
+];
