7469d973f9a0d1edf1a28231890ff4f5190e0902
Michael Gelber <mpgelber7495@gmail.com>
2019-11-27 11:44:48 -0500

    Mobile responsive band member page

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,8 +1,9 @@
 $(".member-headshot").click(function(event) {
   var name = event.target.id;
-  $(".member-headshot").css("opacity", ".1");
-  $(`#${name}`).css("opacity", "0.8");
+  $(".member-headshot").css("opacity", ".2");
+  $(`#${name}`).css("opacity", "0.9");
   $(".member-headshot").addClass("inactive-member");
+  $(`#${name}`).removeClass("inactive-member");
   var memberObject = members.filter(member => member["name"] === name)[0];
   var memberElement = $(".selected-character-content-holder");
   memberElement.html("");
@@ -11,6 +12,8 @@ $(".member-headshot").click(function(event) {
 
   var memberInstruments = `<div class = "row"> <div class="col-12 d-flex justify-content-center"><h3 class = "member-instrument-active"> Instruments: ${memberObject["instrument"]}</h3> </div></div>`;
   memberElement.append(memberInstruments);
-  var memberPic = `<div class = "row"> <div class="col-12 d-flex justify-content-center"> <img src = ${memberObject["actionShotURL"]} class = "w-50"> </div> </div>`;
+  var memberPic = `<div class = "row"> <div class="col-12 d-flex justify-content-center"> <img src = ${memberObject["actionShotURL"]} class = "member-image-active"> </div> </div>`;
   memberElement.append(memberPic);
+  var memberBio = `<div class = "row"> <div class="col-12 mt-3 d-flex justify-content-center"><p class = "text-center member-bio-active">${memberObject["bio"]}</p> </div></div>`;
+  memberElement.append(memberBio);
 });
--- assets/javascript/bandData.js
+++ assets/javascript/bandData.js
@@ -4,27 +4,27 @@ var members = [
     instrument: "Guitar and Vocals",
     actionShotURL: "./assets/images/rafi-action.png",
     bio:
-      "Rafi is a true musical prodigy. He's been playing every instrument since the day he was born. Rafi's passions are rooted in the funk but he gets groovy to all forms of music."
+      "Rafi is a true musical prodigy. Inspired by Marty McFly, he's been playing every instrument since the day he saw Back to the Future. Rafi's passions are rooted in the funk but he gets groovy to all forms of music."
   },
   {
     name: "Paul",
     instrument: "Drums and Vocals",
     actionShotURL: "./assets/images/paul-action.png",
     bio:
-      "Rafi is a true musical prodigy. He's been playing every instrument since the day he was born. Rafi's passions are rooted in the funk but he gets groovy to all forms of music."
+      "Paul has been playing drums for years. He's more reliable than a metronome when behind the kit but he also loves to take on projects behind the screen on ableton. Paul's a talented song writter who aspires to be Matty Healy."
   },
   {
     name: "Mikey",
     instrument: "Bass and Banter",
     actionShotURL: "./assets/images/mikey-action.png",
     bio:
-      "Rafi is a true musical prodigy. He's been playing every instrument since the day he was born. Rafi's passions are rooted in the funk but he gets groovy to all forms of music."
+      "Mikey started playing bass with the inception of the band. He was taught by Rafi and now lays down solid rhythms. Mikey is most well known for his lack of cables as he traverses the crevasses of the venues using his trusty wireless system. "
   },
   {
     name: "Eric",
     instrument: "Saxophone",
     actionShotURL: "./assets/images/eric-action.png",
     bio:
-      "Rafi is a true musical prodigy. He's been playing every instrument since the day he was born. Rafi's passions are rooted in the funk but he gets groovy to all forms of music."
+      "Eric is a true master of trades. His jazz-based upbringing leaves him selecting only the most well thought out notes that propell TYSO's tunes into the rock n' roll atmosphere. "
   }
 ];
