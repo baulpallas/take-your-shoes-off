17c5f3bb050fa6182a3d8a0740561ef31aff0d2f
Michael Gelber <mpgelber7495@gmail.com>
2019-11-27 17:16:46 -0500

    Added width constraint to member images

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -12,7 +12,7 @@ $(".member-headshot").click(function(event) {
 
   var memberInstruments = `<div class = "row"> <div class="col-12 d-flex justify-content-center"><h3 class = "member-instrument-active"> Instruments: ${memberObject["instrument"]}</h3> </div></div>`;
   memberElement.append(memberInstruments);
-  var memberPic = `<div class = "row"> <div class="col-12 d-flex justify-content-center"> <img src = ${memberObject["actionShotURL"]} class = "member-image-active"> </div> </div>`;
+  var memberPic = `<div class = "row"> <div class="col-12 d-flex justify-content-center"> <img src = ${memberObject["actionShotURL"]} class = "member-image-active w-75"> </div> </div>`;
   memberElement.append(memberPic);
   var memberBio = `<div class = "row"> <div class="col-12 mt-3 d-flex justify-content-center"><p class = "text-center member-bio-active">${memberObject["bio"]}</p> </div></div>`;
   memberElement.append(memberBio);
